f5b25366f8306e3d0dca03e3b388f0243691e9ed
amon <amon@nandynarwhals.org>
2017-02-01 05:17:52 +0800

    Made the feedbackToUser field private in CommandResult and create a getter.

--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -11,7 +11,14 @@ import java.util.Optional;
 public class CommandResult {
 
     /** The feedback message to be shown to the user. Contains a description of the execution result */
-    public final String feedbackToUser;
+    private final String feedbackToUser;
+
+    /**
+     * @return the feedbackToUser
+     */
+    public String getFeedbackToUser() {
+        return feedbackToUser;
+    }
 
     /** The list of persons that was produced by the command */
     private final List<? extends ReadOnlyPerson> relevantPersons;
